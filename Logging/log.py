8f699eb42e4046e232feb6fc5ae7f23845874a47
Muesli Torri <mueslito@an-b3.tash.21-school.uz>
2025-09-24 10:38:36 +0500

    use log

---
+++ Logging/log.py
@@ -0,0 +1,9 @@
+import logging 
+
+logging.basicConfig(level = logging.DEBUG)
+
+logging.debug("BU debug xabar (odatda muammolarni izlash uchun).")
+logging.info("Dastur ishlashni boshladi.")
+logging.warning("Bu ogohlantirish, lekin dastur ishlashni davom etadi.")
+logging.error("Xatolik yuz berdi!")
+logging.critical("Juda katta xatolik!!!")
